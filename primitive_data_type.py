298ba55982802c885093e276c500c9860fff68f5
akashvicky23 <akashvicky23@gmail.com>
2026-04-19 22:07:09 +0530

    Primitive type

---
+++ primitive_data_type.py
@@ -0,0 +1,14 @@
+
+legs : int = 4
+ears : int = 2
+color : str = "Goldendoodle"
+age : int = 5
+color : str = "Yellow"
+speak : bool = True
+
+print(legs)
+print(ears)
+print(color)
+print(age)
+print(color)
+print(speak)
